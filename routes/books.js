1d45453bc8ddd903ce0272240dde88c8addd200f
ThomasA64 <thomas.arthur64@gmail.com>
2021-04-26 08:54:57 -0400

    created books/show

--- routes/books.js
+++ routes/books.js
@@ -52,10 +52,11 @@ router.post("/", async (req, res) => {
   }
 });
 
+// Sow Book Route
 router.get("/:id", async (req, res) => {
   try {
     const book = await Book.findById(req.params.id).populate("author").exec();
-    res.render("books.show", { book: book });
+    res.render("books/show", { book: book });
   } catch {
     res.redirect("/");
   }
