6d8c1f1510ae9a09582e2c210c3f7a4ddca047a6
ThomasA64 <thomas.arthur64@gmail.com>
2021-04-26 11:30:53 -0400

    changed title to Library

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
-
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
 const bookRouter = require("./routes/books");
-const methodOverride = require("method-override");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
@@ -21,12 +20,7 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 const mongoose = require("mongoose");
-mongoose
-  .connect(process.env.DATABASE_URL, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  })
-  .catch((error) => console.error(error));
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Mongoose"));
